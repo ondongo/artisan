28daf84bdef4364c2a60b05f43f7c465ebeff195
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-12-18 13:36:28 +0100

    style : integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,117 @@
-import Image from "next/image";
+"use client";
+
+import { Button, Flex, Text, Image, Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [fileData, setFileData] = useState<{
+    base64: string | null;
+    name: string | null;
+    type: string | null;
+  }>({
+    base64: null,
+    name: null,
+    type: null,
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const onDrop = (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFileData({
+          base64: reader.result?.toString().split(",")[1] || null,
+          name: file.name,
+          type: file.type,
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    maxSize: 5 * 1024 * 1024,
+  });
+
+
+  const handleSubmit = async () => {
+    if (!fileData.base64) {
+      alert("Veuillez télécharger un fichier.");
+      return;
+    }
+
+    console.log("filedata", fileData.base64);
+
+    const res = await fetch("/api/analyze", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        imageBase64: fileData.base64 
+      }),
+ 
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+      alert(`Texte extrait : ${data.extractedText}`);
+    } else {
+      alert(`Erreur : ${data.message}`);
+    }
+  };
+
+  return (
+    <Flex height={"100vh"} justifyContent={"center"} alignItems={"center"}>
+      <Flex direction="column" align="center" width="100%">
+        <div
+          {...getRootProps()}
+          style={{
+            border: "2px dashed #319795",
+            padding: "20px",
+            borderRadius: "8px",
+            cursor: "pointer",
+            width: "100%",
+            maxWidth: "400px",
+            textAlign: "center",
+          }}
+        >
+          <input {...getInputProps()} />
+          <Text mb={2}>
+            Déposez votre devis ici, ou cliquez pour le sélectionner (pdf, png,
+            jpeg jusqu&nbsp;à 5MB)
+          </Text>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        {/* Affichage de l'aperçu ou du nom du fichier sélectionné */}
+        {fileData.type?.startsWith("image/") && fileData.base64 ? (
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={`data:image/jpeg;base64,${fileData.base64}`}
+            alt="Aperçu du fichier"
+            maxWidth="300px"
+            mt={4}
+            borderRadius="8px"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        ) : fileData.type === "application/pdf" ? (
+          <Box mt={4}>
+            <Text fontSize="xl" fontWeight="bold">
+              PDF sélectionné : {fileData.name}
+            </Text>
+            {/* Optionnellement, tu pourrais afficher une icône PDF ici */}
+          </Box>
+        ) : fileData.name ? (
+          <Text mt={4}>Fichier sélectionné : {fileData.name}</Text>
+        ) : null}
+
+        <Flex mt={4}>
+          <Button onClick={handleSubmit} colorScheme="teal">
+            Soumettre le devis pour analyse
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
